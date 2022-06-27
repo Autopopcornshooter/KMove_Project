5a01d9e151779bcf694edb692e57305d73a1165a
SiyunParkinGit <firetruck433@gmail.com>
2022-06-27 16:18:35 +0900

    하하

--- KMove_Project/Assets/Script/PlayerCtrl.cs
+++ KMove_Project/Assets/Script/PlayerCtrl.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-
+    [SerializeField] private float moveSpeed;
+    Rigidbody rgd;
     Vector3 move_dir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rgd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         move_dir.x = Input.GetAxis("Horizontal");
+        rgd.
     }
 }
