a1d1395c412f0d6e38703acb0c6ad404fc01367c
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2022-09-26 19:17:41 +0900

    캐릭터에 히트박스 시스템 적용
    먼저 맞는 히트박스만 처리 가능하도록 함

--- KMove_Project/Assets/Script/L_Script/L_HItboxScript.cs
+++ KMove_Project/Assets/Script/L_Script/L_HItboxScript.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 public class L_HitboxScript : MonoBehaviour
 {
-   Animator animator;
+    [SerializeField]
+  private Animator animator;
+    private BoxCollider collider;
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider>();
+        collider.enabled = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Enemy_Attack();
+        }
+    }
+    private void Enemy_Attack()
+    {
+        animator.SetTrigger("Attack");
+        StartCoroutine(HitboxEnable());
+    }
+    IEnumerator HitboxEnable()
+    {
+        yield return new WaitForSecondsRealtime(0.3f);
+        collider.enabled = true;
+        yield return new WaitForSecondsRealtime(0.5f);
+        collider.enabled = false;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag != "Untagged")
         {
-            Debug.Log("Player Damaged!");
-        }
-        else if(other.gameObject.tag == "Sheild")
-        {
-            Debug.Log("Sheild hit!");
-            animator.SetTrigger("Parried");
-        }
-        else if (other.gameObject.tag == "Sword")
-        {
-            Debug.Log("Sword hit!");
-            animator.SetTrigger("Blocked");
+            collider.enabled = false;
+            if (other.gameObject.tag == "Player")
+            {
+                Debug.Log("Player Damaged!");
+            }
+            else if (other.gameObject.tag == "Sheild")
+            {
+                Debug.Log("Sheild hit!");
+                animator.SetTrigger("Parried");
+            }
+            else if (other.gameObject.tag == "Sword")
+            {
+                Debug.Log("Sword hit!");
+                animator.SetTrigger("Blocked");
 
+            }
         }
     }
 }
